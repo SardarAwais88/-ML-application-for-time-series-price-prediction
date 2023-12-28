1e52825c83d4efb0b8eeabc1890bbfd8f7c91301
SardarAwais88 <khanowais8888@gmail.com>
2023-12-28 17:00:31 +0500

    first commit

---
+++ app2.py
@@ -0,0 +1,94 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+import plotly.express as px
+
+# Function to train the model
+def train_model(data):
+    # Assume your time series data has 'Date' and 'Close' columns
+    data['Date'] = pd.to_datetime(data['Date'])
+    data = data.sort_values('Date')
+
+    # Feature engineering: extracting date-related features
+    data['Year'] = data['Date'].dt.year
+    data['Month'] = data['Date'].dt.month
+    data['Day'] = data['Date'].dt.day
+    data['DayOfWeek'] = data['Date'].dt.dayofweek
+
+    # Split data into features and target variable
+    X = data[['Year', 'Month', 'Day', 'DayOfWeek']]
+    y = data['Close']  # Assuming 'Close' is the target variable
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train a simple Random Forest Regressor as an example
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    # Make predictions on the test set
+    predictions = model.predict(X_test)
+
+    # Display evaluation metrics
+    mse = mean_squared_error(y_test, predictions)
+    st.write(f'Mean Squared Error: {mse:.2f}')
+
+    # Display a plot of actual vs predicted prices
+    fig = px.scatter(x=y_test.index, y=y_test, labels={'y': 'Actual Price'}, title='Actual vs Predicted Prices')
+    fig.add_trace(px.scatter(x=y_test.index, y=predictions, labels={'y': 'Predicted Price'}).data[0])
+    st.plotly_chart(fig)
+
+    return model
+
+# Function to make predictions
+def predict_price(model, input_features):
+    # Assuming input_features is a dictionary containing Year, Month, Day, DayOfWeek
+    input_data = pd.DataFrame([input_features])
+    prediction = model.predict(input_data)
+    return prediction[0]
+
+# Streamlit UI
+# Streamlit UI
+# ...
+
+# Streamlit UI
+# ...
+
+# Streamlit UI
+# Streamlit UI
+def main():
+    st.title('Time Series Price Prediction App')
+
+    # Upload data
+    uploaded_file = st.file_uploader('Upload your time series data (CSV file)', type=['csv'])
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+
+        # Train the model
+        st.header('Train the Model')
+        trained_model = train_model(data)
+
+        # Prediction
+        st.header('Make Predictions')
+        input_features = st.form(key='prediction_form').columns(4)
+        for col in ['Year', 'Month', 'Day', 'DayOfWeek']:
+            # Use indexing to access each input element
+            input_features[0][col].number_input(f'Enter {col}', value=2023)
+
+        # Check if the form is submitted
+        if st.form_submit_button('Predict'):
+            # Create a dictionary to store the input features
+            input_data = {col: input_features[0][col].number_input(f'Enter {col}', value=2023) for col in ['Year', 'Month', 'Day', 'DayOfWeek']}
+            prediction = predict_price(trained_model, input_data)
+            st.success(f'Predicted Price: {prediction:.2f}')
+
+# ...
+
+# ...
+
+# Run the app
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,37 @@
+Time Series Price Prediction App
+This is a Streamlit web application for time series price prediction. It allows users to upload a time series dataset in CSV format, trains a Random Forest Regressor model, and provides predictions based on user-input features.
+
+Features
+Upload Data: Users can upload their time series data in CSV format.
+Train the Model: The app automatically extracts date-related features and trains a Random Forest Regressor.
+Make Predictions: Users can input specific date-related features to get predictions from the trained model.
+Getting Started
+Follow these instructions to get a copy of the project up and running on your local machine.
+
+Prerequisites
+Python installed
+pip (Python package installer) installed
+Installation
+Clone the repository:
+
+bash
+Copy code
+git clone https://github.com/your-username/your-repo.git
+Navigate to the project directory:
+
+bash
+Copy code
+cd your-repo
+Install the required packages:
+
+bash
+Copy code
+pip install -r requirements.txt
+Usage
+Run the Streamlit app:
+
+bash
+Copy code
+streamlit run app.py
+Visit http://localhost:8501 in your web browser to access the app.
+
